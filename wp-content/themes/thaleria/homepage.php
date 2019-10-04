7696267a00b8180336ffcaa1b3d939ff8e4de055
MKA1500 <maciejkabsch@gmail.com>
2019-10-04 12:26:03 +0200

    menu, initial posts index

--- wp-content/themes/thaleria/homepage.php
+++ wp-content/themes/thaleria/homepage.php
@@ -26,10 +26,60 @@ get_header();
 </header><!-- /.header -->
 <main class="main-content bg-royal-blue">
   <?php
-    $page = get_page_by_title( 'home' );
-    $content = apply_filters('the_content', $page->post_content);
-    echo $content;
+  $page = get_page_by_title( 'home' );
+  $content = apply_filters('the_content', $page->post_content);
+  echo $content;
   ?>
+  <div class="job-posts-index">
+    <div class="row">
+      <?php
+      $counter = 1;
+      $args = array(
+        'post_type' => 'post',
+        'post_status' => 'publish',
+        'category_name' => 'Job Post',
+        'posts_per_page' => 5
+      );
+      $arr_posts = new WP_Query( $args );
+      if ( $arr_posts->have_posts() ) :
+      while ( $arr_posts->have_posts() ) :
+      $arr_posts->the_post();
+      ?>
+      <article id="post-<?php the_ID(); ?>" class="col-xl-8 post_<?php echo $counter; ?>">
+        <a href="<?php echo get_permalink( $post->ID ); ?>">
+          <div class="frontpage-thumbnail">
+            <?php
+            if ( has_post_thumbnail() ) :
+            the_post_thumbnail();
+            endif;
+            ?>
+          </div>
+          <div class="index-text">
+            <header class="entry-header">
+              <h2 class="entry-title"><?php the_title(); ?></h2>
+            </header>
+            <div class="entry-content">
+              <p>
+                <?php echo get_the_excerpt(); ?>
+                <strong>Czytaj więcej &raquo</strong>
+              </p>
+            </div>
+          </div>
+        </a>
+      </article>
+      <?php
+      $counter++;
+      endwhile;
+      endif;
+      ?>
+    </div>
+    <div class="see-more">
+      <a href="<?php echo get_option("siteurl") .'/blog'; ?>" role="button" class="btn btn-default">
+        Zobacz więcej
+        <i class="fa fa-angle-right"></i>
+      </a>
+    </div>
+  </div>
 </main>
 
 <?php
